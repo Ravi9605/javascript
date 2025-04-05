641eaf8b15bc8b5fd8d1dff478dd379d5fac24c3
Ravi9605 <binjhaderavi04@gmail.com>
2025-04-05 07:30:00 +0000

    datatypesummary

---
+++ 01_basic/04_compaire.js
@@ -0,0 +1,17 @@
+// console.log("2">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null >= 0);
+
+console.log(2=='2');// eqallity with type conversion
+console.log(2==='2');//equallity check without type conversion
+console.log(2!='2');
+console.log(2!=='2');
+
+
+
+
+
+
+
+
