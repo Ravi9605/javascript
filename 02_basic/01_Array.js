cf58195c5ff29830220865aa9f6b2409fcfec087
Ravi9605 <binjhaderavi04@gmail.com>
2025-04-05 18:04:51 +0000

    array

---
+++ 02_basic/01_Array.js
@@ -0,0 +1,54 @@
+const myarr = [1,2,3,4,5]
+// console.log(myarr[4]);
+const myarr2 = new Array(1 , 2 ,4 ,6);
+// console.log(myarr[0]);
+// console.log(myarr.length);
+//Array methods
+// myarr.push(60);
+// console.log(myarr);
+// console.log(`Removed value is --> ${myarr.pop()}`);
+
+// console.log(myarr);
+// console.log(myarr.length);
+
+// myarr.unshift(100)
+// console.log(myarr);
+// myarr.shift()
+// console.log(myarr);
+// console.log(myarr.includes(4));
+// console.log(myarr.indexOf(4));
+
+// const newarr = myarr.join()
+// console.log(myarr);
+// console.log(typeof myarr);
+
+// console.log(newarr);
+// console.log(typeof newarr);
+
+//slice and splice
+
+// const myn1 = myarr.slice(1,3)
+// console.log("A" , myarr);//orignal array
+ 
+// console.log(myn1);
+// console.log("B" ,myarr);
+
+// const myn2 = myarr.splice(1,3)
+// console.log("C" , myarr);
+
+// console.log(myn2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
