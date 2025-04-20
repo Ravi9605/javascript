0f29d5fa383bbdb3eb736b8fc18c46fc8bca6397
Ravi9605 <binjhaderavi04@gmail.com>
2025-04-20 09:44:12 +0000

    oops

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,38 @@
+
+// ObjectLiterals
+// const user = {
+//     "username" : "Ravi",
+//     "logincount" : 8,
+//      "signedin" : true,
+//      getuserdetail: function(){
+//             // console.log(`this is user --> ${this.username}` );
+//             // console.log(this);
+            
+            
+//      }
+// }
+
+// console.log(user["username"]);
+// // console.log(user.getuserdetail())
+// console.log(this);
+
+//constructor function
+// function User(username, logincount, isloggedin ){
+//     this.username = username,
+//     this.logincount = logincount,
+//     this.isloggedin = isloggedin
+//     this.greeting = function(){
+//         console.log(`Welcome ${this.username}`);
+        
+//     }
+//     return this;
+
+// }
+
+// const Userone = new  User("Ravi ", 18, true)
+// const usertwo = new User("Chaiaurcode", 25, false)
+// console.log(Userone instanceof User);
+// console.log(usertwo);
+
+
+
